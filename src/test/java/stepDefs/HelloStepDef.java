738ef49be05824acca66fb1421e948e8ed87d32f
Gautam Deka <de.gautam19@gmail.com>
2018-07-10 22:39:34 +0530

    changes from dev branch

--- src/test/java/stepDefs/HelloStepDef.java
+++ src/test/java/stepDefs/HelloStepDef.java
@@ -29,6 +29,7 @@ public class HelloStepDef {
 	public void i_have_dollars_with_me(int arg1) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 		System.out.println(arg1);
+		System.out.println(arg1);
 	}
 	
 	@Given("^I want to write a step with precondition$")
@@ -38,12 +39,29 @@ public class HelloStepDef {
 			System.out.println("From Step11111 "+objData.getData().getField("Data1"));	
 		}
 			    
-	}	
-		
+	}
+	
+	@Given("^I want to write a step with precondition1$")
+	public void i_want_to_write_a_step_with_precondition1() throws Throwable {
+		System.out.println("I want to write a step with precondition");
+		while (objData.getData().next()) {
+			System.out.println("From Step11111 "+objData.getData().getField("Data1"));	
+		}
+			    
+	}
+	
 	@When("^User check for TestData$")
 	public void user_check_for_TestData() throws Throwable {
 		System.out.println("User check for TestData");
 		System.out.println("From Step1 "+objData.getData().getField("Data1"));
+		System.out.println("User check for TestData");
+		
+	}
+	@When("^User check for TestData123$")
+	public void justtotestdeleteAfterSometime() throws Throwable {
+		System.out.println("User check for TestData");
+		System.out.println("From Step1 "+objData.getData().getField("Data1"));
+		System.out.println("User check for TestData");
 		
 	}	
 	
